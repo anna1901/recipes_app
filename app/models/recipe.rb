8fc03d11d0b86480e0d2589076aada50ec8bc91e
Anna Olak <anna.olak1901@gmail.com>
2019-08-10 16:56:22 +0200

    Changed recipe form to recive a button_text variable

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,3 +1,7 @@
 class Recipe < ApplicationRecord
+  validates :name, presence: true, length: { maximum: 60 }
+  validates :description, presence: true, length: { maximum: 255 }
+  validates :category, presence: true
+
   belongs_to :user
 end
