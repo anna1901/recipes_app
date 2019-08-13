c9eda807283199d6334a61464e1cf93dc42ee53a
Anna Olak <anna.olak1901@gmail.com>
2019-08-13 17:21:34 +0200

    Change recipe model so that recipes get destroyed when user delets his account

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -2,6 +2,7 @@ class Recipe < ApplicationRecord
   validates :name, presence: true, length: { maximum: 60 }
   validates :description, presence: true, length: { maximum: 255 }
   validates :category, presence: true
+  validates :user_id, presence: true
 
   belongs_to :user
 end
